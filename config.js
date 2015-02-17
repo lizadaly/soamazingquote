14f53e9cfc4c6e7d1099bafb940c28ddb3079bd5
Liza Daly <lizadaly@gmail.com>
2015-02-16 22:16:11 -0500

    Import

---
+++ config.js
@@ -0,0 +1,7 @@
+// Put your own Twitter App keys here. See README.md for more detail.
+module.exports = {
+    consumer_key:         'Z2guK5vam4q38LDuczqurSeyW',
+    consumer_secret:      'NkXHNcHGY4h5FabceGs0wbeKbFsJnzQmQwqMUI61rbln89mY0U',
+    access_token:         '3041192247-KkYvUgg9A8Sg3yib4Ij71qQSf8L7m3BumTQiYoT',
+    access_token_secret:  'UrkrjmMpR0CurNHL65nBJr9v1AygDBiP2xsiuPcirAx06'
+};
